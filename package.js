eca115ee55e1cc083e389171717760dbaa727534
Mike Schäkermann <m.schaekermann@spontaneousorder.de>
2013-09-05 12:37:26 -0400

    initial release 0.1.0

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+      summary: "Easily create charts with Highcharts."
+});
+
+Package.on_use(function (api) {
+	api.use('jquery', 'client');
+    api.add_files('lib/highcharts.js', 'client');
+});
